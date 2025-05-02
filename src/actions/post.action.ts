ed46b10525680e1720521821bbc8cab224e129f0
Riya Rastogi <riya02rastogi@gmail.com>
2025-05-02 06:48:19 +0530

    get diff posts at home page

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -4,22 +4,73 @@ import prisma from "@/lib/prisma";
 import { getDbUserId } from "./user.action";
 import { revalidatePath } from "next/cache";
 
-export async function createPost(content: string, image: string){
+export async function createPost(content: string, image: string) {
     try {
         const userId = await getDbUserId();
         if (!userId) return;
         const post = await prisma.post.create({
-            data:{
+            data: {
                 content,
                 image,
-                authorId: userId, 
+                authorId: userId,
             },
         });
         revalidatePath("/"); // purge the cache for the home page
         return { success: true, post };
-        
+
     } catch (error) {
         console.error("Failed to create post:", error);
         return { success: false, error: "Failed to create post" };
+    }
+}
+
+export async function getPosts() {
+    try {
+        const posts = await prisma.post.findMany({
+            orderBy: {
+                createdAt: "desc",
+            },
+            include: {
+                author: {
+                    select: {
+                        id: true,
+                        name: true,
+                        image: true,
+                        username: true,
+                    },
+                },
+                comments: {
+                    include: {
+                        author: {
+                            select: {
+                                id: true,
+                                username: true,
+                                image: true,
+                                name: true,
+                            },
+                        },
+                    },
+                    orderBy: {
+                        createdAt: "asc",
+                    },
+                },
+                likes: {
+                    select: {
+                        userId: true,
+                    },
+                },
+                _count: {
+                    select: {
+                        likes: true,
+                        comments: true,
+                    },
+                },
+            },
+        });
+
+        return posts;
+    } catch (error) {
+        console.log("Error in getPosts", error);
+        throw new Error("Failed to fetch posts");
     }
 }

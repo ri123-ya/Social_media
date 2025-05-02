000d439c2c0d7f83a9830662c4cc12606050e2f0
Riya Rastogi <riya02rastogi@gmail.com>
2025-05-02 06:21:53 +0530

    app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/ThemeProvider"
 import  Navbar  from "@/components/Navbar"
-
-
+import Sidebar  from "@/components/Sidebar"
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -48,13 +48,14 @@ export default function RootLayout({
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                     <div className="hidden lg:block lg:col-span-3">
-                      Sidebar 
+                      <Sidebar />
                     </div>
                     <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
               </main>
             </div>
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
